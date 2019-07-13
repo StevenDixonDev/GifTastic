7bb280ec317e367673bb16c0f98c2c9d8ffbdf01
Steven Dixon <stevendixon112@gmail.com>
2019-07-13 12:03:06 -0400

    updated layout and added new button

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -7,51 +7,65 @@
   - Passing arrays through the trigger event destructures them
   - I understand how $(this) works in Jquery now 
   - Bootstrap ordering
-
 */
-$(document).ready(function(){
-
+$(document).ready(function () {
   // create array for buttons
-  const buttons = ["arm bird", "crazy", "WTF"];
+  let buttons = [];
+  // get previous buttons from local storage
+  let previousButtons = localStorage.getItem("buttons");
+  // check if there were any previous buttons
+  if (Array.isArray(JSON.parse(previousButtons))) {
+    // if there were assign them to buttons
+    buttons = JSON.parse(previousButtons);
+  } else {
+    // otherwise use defaults
+    buttons = ["JavaScript", "CSS", "HTML", "Typing"];
+  }
+
   // function that subscribes custom events to document
   documentSubscribe(document);
   // handle adding new button to buttons array 
-  $('#new-button').on('click', function(e){
+  $('#new-button').on('click', function (e) {
     e.preventDefault();
-    if($('#new-button-text').val() !== ""){
+    if ($('#new-button-text').val() !== "") {
       // add text from input into button array
       buttons.push($('#new-button-text').val());
       // reset text in input feild
       $('#new-button-text').val("");
       // let the dom know the buttons were changes
       $(document).trigger('update-buttons', [buttons]);
+      // clear localstorage
+      localStorage.clear();
+      // write new buttons to local storage
+      localStorage.setItem("buttons", JSON.stringify(buttons));
     }
   });
 
-  $('#size-button').on('click', function(e){
+  // trigger size change
+  $('#size-button').on('click', function (e) {
     e.preventDefault();
     $(document).trigger('change-size');
   })
 
   // trigger event when gif is clicked on
-  $(document).on('click', '.gifs', function(e){
-      e.preventDefault();
-      $(document).trigger('gif-click', $(this));
+  $(document).on('click', '.gifs', function (e) {
+    e.preventDefault();
+    $(document).trigger('gif-click', $(this));
   })
 
   // trigger event when the buttons are clicked on
-  $(document).on('click', '.custom-button', function(e){
+  $(document).on('click', '.custom-button', function (e) {
     e.preventDefault();
     $(document).trigger('button-trigger', $(this).text());
   })
 
   // render buttons for the first time
   $(document).trigger('update-buttons', [buttons]);
-  $("#size-button").text($("#size-button").data('size'))
+  $("#size-button").text($("#size-button").data('size'));
 });
 
 
-function documentSubscribe(el){
+function documentSubscribe(el) {
   // adds custom events to DOM
   $(el).on('update-buttons', renderButtons);
   $(el).on('update-images', updateImages);
@@ -60,60 +74,58 @@ function documentSubscribe(el){
   $(el).on('change-size', changeImageSize);
 }
 
-function changeImageSize(event, data){
+function changeImageSize(event, data) {
   const sizes = ["fixed_width", "fixed_height", "downsized", "original"];
   let currentSize = $("#size-button").data('size');
   let index = sizes.indexOf(currentSize);
-
-  index+=1;
-
-  if(index >= sizes.length){
+  // increase the index
+  index += 1;
+  // make sure the index is not greater if it is set it to 0 
+  if (index >= sizes.length) {
     index = 0;
   }
-
+  // set the buttons attributes 
   $("#size-button").data('size', sizes[index]);
   $("#size-button").text(sizes[index]);
 }
 
-function search(event, thingToSearchFor){
+function search(event, thingToSearchFor) {
   // ajax request to giphy for gifs
   $.ajax({
-    url: `https://api.giphy.com/v1/gifs/search?q=${thingToSearchFor}&limit=10&api_key=CcLtxrF0OzF1nH4I4jlUfsOp4TBYkmpT`,
+    url: `https://api.giphy.com/v1/gifs/search?q=${thingToSearchFor}&limit=10&rating=pg&api_key=CcLtxrF0OzF1nH4I4jlUfsOp4TBYkmpT`,
     method: 'get'
-  }).then((res)=>{
+  }).then((res) => {
     // tell the dom the data has been found
-    console.log(res)
     $(document).trigger('update-images', res);
   });
 }
 
-function updateImages(event, data){
+function updateImages(event, data) {
   // clear the images from the dom
   $('#gif-location').empty();
   let currentSize = $("#size-button").data('size');
   // add new images
-  $.each(data.data, function(index, gif){
+  $.each(data.data, function (index, gif) {
     $('#gif-location').append(`
-      <img class="gifs  m-1 p-1" data-gif-src="${gif.images[currentSize].url}" src="${gif.images[currentSize+'_still'].url}"/>
+      <img class="gifs  m-1 p-1" data-gif-src="${gif.images[currentSize].url}" src="${gif.images[currentSize + '_still'].url}"/>
     `)
   })
 }
 
-function renderButtons(event, buttons){
+function renderButtons(event, buttons) {
   // clear the buttons from the dom
   $('#button-area').empty();
   // append buttons to the button area
-  $.each(buttons, function(index, button){
-    $('#button-area').append(`<button class="btn btn-secondary btn-lg btn-block custom-button">${button}</button>`);
+  $.each(buttons, function (index, button) {
+    $('#button-area').append(`<button class="btn btn-outline-info btn-lg btn-block custom-button">${button}</button>`);
   });
 }
 
-function handleGifClick(event, element){
+function handleGifClick(event, element) {
   // get the current gif src
   let gif = $(element).data().gifSrc;
   // get the current still image
   let still = $(element).attr('src');
-
   // swap them
   $(element).data("gifSrc", still);
   $(element).attr('src', gif);
